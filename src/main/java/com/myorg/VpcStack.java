6f4acd0d58ceecede2297dbb36eb46d6876d704b
Ricardo Gusi <ricardogusi@hotmail.com>
2025-05-26 18:23:54 -0300

    criação da stack vpc e cluster

--- src/main/java/com/myorg/VpcStack.java
+++ src/main/java/com/myorg/VpcStack.java
@@ -1,4 +1,27 @@
 package com.myorg;
 
-public class VpcStack {
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.constructs.Construct;
+
+public class VpcStack extends Stack {
+
+    final private Vpc vpc;
+
+    public VpcStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public VpcStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        vpc = Vpc.Builder.create(this, "Vpc01")
+                .maxAzs(3) //Zonas de disponibilidade que a VPC vai atuar
+                .build();
+    }
+
+    public Vpc getVpc() {
+        return vpc;
+    }
 }
